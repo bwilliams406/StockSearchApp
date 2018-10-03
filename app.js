d009b32bf900eee551f820c5d4b6cf6495f62940
SupaPist <brodywilliams422@gmail.com>
2018-10-02 22:46:50 -0500

    updated js and css

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-$(document).ready(function () {
-  const validationList = []
+ const validationList = []
   const stocks = []
   const queryAllSymbols = `https://api.iextrading.com/1.0/ref-data/symbols`;
   $.ajax({
@@ -11,21 +10,25 @@ $(document).ready(function () {
       validationList.push(validList);
     };
   });
-  $('#btnSubmit').on('click', function () {
+
+
+  $('#btnSubmit').on('click', function (event) {
     event.preventDefault();
     let newStock = $('#stockInfo').val().toUpperCase();
     if (!validationList.includes(newStock)) {
       alert("Please input a valid stock symbol.");
     } else {
       stocks.push(newStock);
-      $('#stockInfo').empty();
+      $('#stockInfo').val('');
       render();
     };
   });
+
   $('#btnClear').on('click', function () {
     $('#tBody').empty();
   });
-  $("#stocks-view").on("click", "input", function () {
+
+  $("#stocks-view").on("change", function () {
     let stockSymbol = this.value;
     const queryURL1 = `https://api.iextrading.com/1.0/stock/${stockSymbol}/quote`;
     const queryURL2 = `https://api.iextrading.com/1.0/stock/${stockSymbol}/logo`;
@@ -48,108 +51,22 @@ $(document).ready(function () {
        method: 'GET'
     }).then(function (response) {
       for (let i = 0; i < response.length; i++) {
-        const Q3 = $('#newsDiv').append(`<h5>${response[i].headline}</h5><br><p>${response[i].datetime}</p><br><p>${response[i].summary}</p>`)
+        const Q3 = $('#newsDiv').append(`<div class='card'><h5>${response[i].headline}</h5><br><p>${response[i].datetime}</p><br><p>${response[i].summary}</p></div>`)
       }
       }) 
      
     });
     $('#tBody').empty();
     $('#logoDiv').empty();
-  });6
+  });
   const render = function () {
     $('#stocks-view').empty();
     for (let i = 0; i < stocks.length; i++) {
       let theId = 'getStocks' + i;
-      $('#stocks-view').append(`<input class="btn btn-outline-secondary" id='${theId}' type='button' value='${stocks[i]}'/>&nbsp`);
-    };
+      $('#stocks-view').append(`<option value='${stocks[i]}'>${stocks[i]}</option>`);
+    };                                
   };
-});
 
 
+  render();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // displaystockInfo function re-renders the HTML to display the appropriate content
-// const displaystockInfo = function () {
-
-//   const stock = $(this).attr('data-name');
-//   const queryURL = `https://api.iextrading.com/1.0/stock/${stock}/batch?types=quote,news&range=1m&last=1`;
-
-//   // Creates AJAX call for the specific stock button being clicked
-//   $.ajax({
-//     url: queryURL,
-//     method: 'GET'
-//   }).then(function(response) {
-//   });
-// }
-
-
-
-// // Function for displaying stock data
-// const render = function () {
-//   $('#buttons-view').empty();
-//   for (let i = 0; i < stocks.length; i++) {
-//     let newButton = $('<button>');
-//       newButton.addClass('stock');
-//       newButton.attr('data-name', stocks[i]);
-//       newButton.text(stocks[i]);
-
-//     $('#buttons-view').append(newButton);
-//   }
-// }
-
-
-
-// const addButton = function(event) {
-
-//   event.preventDefault();
-//   const stock = $('#stock-input').val().trim(); 
-//   stocks.push(stock);
-
-//   $('#stock-input').val('');
-
-//   render();
-// }
-
-// $('#add-stock').on('click', addButton);
-
-// $('#buttons-view').on('click', '.stock', displaystockInfo);
-
-// render();
